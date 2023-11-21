70e8a704f609731fe3ee8a742336d93d772f3497
Long Hello <longhello@Longs-Mac-mini.local>
2023-11-21 17:31:03 +0700

    Add base

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -3,3 +3,6 @@
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
+//volongdev
+//TRrs8xoJkfGGJAq2
+const MONGDB_URI = "mongodb+srv://volong:TRrs8xoJkfGGJAq2@cluster0-volongdev.z1cpsu2.mongodb.net/?retryWrites=true&w=majority"
