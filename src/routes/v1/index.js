09530fc31b58e6b46a02fbdfd7981a25ab192e1c
Long Hello <longhello@Longs-Mac-mini.local>
2023-11-24 17:15:59 +0700

    add router

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -3,3 +3,12 @@
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
+
+import express from 'express'
+
+const router = express.Router()
+
+router.get('/status',(req, res) => {
+    res.status(200).json({message:'APIs V1 are ready to use'})
+
+})
